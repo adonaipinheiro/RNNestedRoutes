d5de1178837df2ce51778394808a2c7485798a9e
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2020-07-22 17:56:21 -0300

    Criando pastas e arquivo routes

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
       justifyContent: 'center',
       alignItems: 'center'
     }}>
-      <Text>Hello World</Text>
+      <Text>Hello World!</Text>
     </View>
   );
 }

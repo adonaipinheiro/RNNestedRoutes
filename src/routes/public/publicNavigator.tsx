3dea5023bdd63ff0c23ec1f158cacf75513e0f15
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2020-07-22 18:12:47 -0300

    Default files

---
+++ src/routes/public/publicNavigator.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const PublicNavigator: React.FC = () => {
+  return <View />;
+}
+
+export default PublicNavigator;
---
+++ src/screens/public/LogIn/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const LogIn: React.FC = () => {
+  return <View />;
+}
+
+export default LogIn;

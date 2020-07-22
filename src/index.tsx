75fb6687ea84193ac4760bcd312cc658e746164a
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2020-07-22 17:35:23 -0300

    Initial Commit

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+export default App;
